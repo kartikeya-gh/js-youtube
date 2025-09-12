afe3517c04f13b7f50e4b9293c7bcbc4ce1ad511
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-12 10:00:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "kartik"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(NOT AN NUMBER)
+// true => 1, false => 0
+// null => 0
+// undefined => NaN
+// "string" => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+//"kartik" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
