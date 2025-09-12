a3c667c7f4cd2f370155b1e00ef5675d207fc579
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-12 10:32:42 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,4 +25,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+// *************************************Operations ************************************//
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+
+let str1 = "hello"
+let str2 = " Kartikeya"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2); // output=12
+//console.log(1 + "2"); //output=12
+//console.log("1" + 2 + 2); //output=122
+//console.log(1 + 2 + "2"); //output=32
+
+//console.log(  (3 + 4) * 5 % 3);
+
+
+
+//console.log(+true); //output=1
+//console.log(+"");   //output=0
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
