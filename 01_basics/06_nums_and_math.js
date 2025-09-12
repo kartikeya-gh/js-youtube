449212ac6342d80daf651e62219862398d70b4cc
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-12 19:28:18 +0000

    num_or_maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,12 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //for 100 to 100.00
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3))
+console.log(otherNumber.toPrecision(3))
