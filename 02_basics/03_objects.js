0386f5f53f84298a2ce319e844aaa9b4200e103b
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-13 17:58:49 +0000

    abc

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,11 @@
+//in js to learn two things are clear like as objects and events...
+
+//to object to declare two ways first is literal and second one is constructor
+//singletone-- create object by constructor is called singleton and this is not create by literal
+
+
+Object.create
+// object literals
+
+
+
