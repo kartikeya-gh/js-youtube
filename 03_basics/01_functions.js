74b200fa6bfbe352e518fc5442b73ea726eb00b9
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-18 17:02:45 +0000

    i

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+
+
+function sayMyName(){
+    console.log("K");
+    console.log("A");
+    console.log("R");
+    console.log("T");
+    console.log("I");
+    console.log("K");
+}
+
+//sayMyName()
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+}
+addTwoNumbers(6,5,6)
