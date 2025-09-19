dd5381ea8c6cf9f98a3e973824932b12bb0f4e85
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-19 18:37:56 +0000

    abc

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,6 +1,6 @@
+//function scope called like [function sayName()]
 
-
-function sayMyName(){
+function sayMyName(){        //function definition called there 
     console.log("K");
     console.log("A");
     console.log("R");
@@ -9,9 +9,21 @@ function sayMyName(){
     console.log("K");
 }
 
-//sayMyName()
+/*sayMyName()  ===//reference of funtion=sayMyName without() ,is called reference butsayName() with() is called as execution of function.
+output=K
+       A
+       R
+       A
+       T
+       I
+       K    */
 
-function addTwoNumbers(number1,number2){
+
+function addTwoNumbers(number1,number2){  //here
     console.log(number1+number2);
 }
-addTwoNumbers(6,5,6)
+
+//if given addTwoNumbers(3,"4") output is ==34
+//if given addTwoNumbers(3,"a") output is ==3a
+//if given addTwoNumbers(3,null) output is ==3
+addTwoNumbers(3,4)   //here in () bracket we have to given argument there
