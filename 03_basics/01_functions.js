d285c3a54a54a4fd0f555d2142afd24b3eaeb05e
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-09-20 05:39:55 +0000

    function and parameter

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,11 +19,67 @@ output=K
        K    */
 
 
-function addTwoNumbers(number1,number2){  //here
+/*function addTwoNumbers(number1,number2){  //here when we make definition of function, in() we given parameters
     console.log(number1+number2);
-}
+} */
 
+//if given addTwoNumbers(3,4) output is 7
 //if given addTwoNumbers(3,"4") output is ==34
 //if given addTwoNumbers(3,"a") output is ==3a
 //if given addTwoNumbers(3,null) output is ==3
-addTwoNumbers(3,4)   //here in () bracket we have to given argument there
+
+//addTwoNumbers(3,4)   //here in () bracket we have to given argument there //can say value there
+
+//const result = addTwoNumbers(3,4)  //save in variable there
+//console.log("Result: ", result);   //output=Result:  undefined
+
+//============================
+
+/* function addTwoNumbers(number1, number2){  
+    let result = number1 + number2
+    //console.log("Kartikeya") //yes this is execute because before return statement 
+    return result
+    //console.log("Kartik") //not execute because after return statement 
+}*/
+//const result = addTwoNumbers(3,5)
+//console.log("Result: ",result); //output=Result:  8
+
+
+
+//another method
+function addTwoNumbers(number1, number2){ 
+    return number1 + number2
+}    
+const result = addTwoNumbers(3,5)
+//console.log("Result: ",result);  //output=Result:  8
+
+
+
+
+/* function loginUserMessage(username){
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage("Kartikeya")); //output=Kartikeya just logged in
+
+console.log(loginUserMessage(""));   //output= just logged in
+console.log(loginUserMessage());     //output=undefined just logged in
+
+*/
+
+function loginUserMessage(username){  //if given (username="sam") this is condition never goes to if condition there and print with no () in value so output is =sam just logged in but with the but after pass the argument with another value like kartik so sam is override and output is kartik just logged in there //
+    if(username===undefined /*in other method we write !username */ ){    //if condition is true there
+    console.log("Please enter a username");
+    return
+    }
+    return `${username} just logged in` //this statement is never read because we use already previous used of return in function
+}
+//console.log(loginUserMessage()); //  output=Please enter a username
+                                   //         undefined
+console.log(loginUserMessage(""));   //output=just logged in
+
+
+
+
+
+
+
