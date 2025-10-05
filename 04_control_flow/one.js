675d89c8de428207599e91f413eb1c159468f78d
Kartikeya Shakya <KartikeyaShakya@gmail.com>
2025-10-05 10:19:23 +0000

    abc

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// if condition
+/* 
+-------------------
+syntax=   
+
+if (condition){
+
+}
+-------------------
+1st.
+if (condition=true){
+    inner code is execute
+}
+--------------------
+2nd.
+if (condition=false){
+    inner code is not execute
+}  
+*/
+
+//comparision operator= <, >, <=, >= ,==, !=, ===
+if (2==="2"){
+    console.log("printed")
+}
+console.log("not printed")
